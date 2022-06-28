5b5caec88c97685d4b2cd3d69789eb6395f133f8
BartekZaczek <bartosz_zaczek@o2.pl>
2022-06-28 20:54:33 +0200

    staticResource as background

--- force-app/main/default/lwc/site/site.js
+++ force-app/main/default/lwc/site/site.js
@@ -2,4 +2,8 @@ import { LightningElement } from 'lwc';
 import GREY from '@salesforce/resourceUrl/Grey';
 export default class Site extends LightningElement {
     greyStaticResource = GREY;
+    
+    get backgroundStyle(){
+        return `height:200px; width:auto; background-image:url(${GREY})`;
+    }
 }
