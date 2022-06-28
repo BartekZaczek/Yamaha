e79dda80bff09d700216af318d7e53b2476011ab
BartekZaczek <bartosz_zaczek@o2.pl>
2022-06-28 21:24:28 +0200

    blue staticResource added as background

--- force-app/main/default/lwc/site/site.js
+++ force-app/main/default/lwc/site/site.js
@@ -1,9 +1,15 @@
 import { LightningElement } from 'lwc';
 import GREY from '@salesforce/resourceUrl/Grey';
+import BLUE from '@salesforce/resourceUrl/Blue';
 export default class Site extends LightningElement {
-    greyStaticResource = GREY;
+
     
-    get backgroundStyle(){
+    get backgroundGrey(){
         return `height:200px; width:auto; background-image:url(${GREY})`;
     }
+
+    get backgroundBlue(){
+        return `height:200px; width:auto; background-image:url(${BLUE})`;
+    }
+
 }
