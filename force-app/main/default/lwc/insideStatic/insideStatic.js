8e5c4fd7237998dc28b72010bf8bb33047c5445f
BartekZaczek <bartosz_zaczek@o2.pl>
2022-06-30 19:10:35 +0200

    added z-index

--- force-app/main/default/lwc/insideStatic/insideStatic.js
+++ force-app/main/default/lwc/insideStatic/insideStatic.js
@@ -1,8 +1,10 @@
 import { LightningElement } from 'lwc';
 import GOOGLEPLAY from '@salesforce/resourceUrl/googlePLay';
 import APPSTORE from '@salesforce/resourceUrl/appStore';
+import RED from '@salesforce/resourceUrl/red';
 
 export default class InsideStatic extends LightningElement {
     imageGoogle = GOOGLEPLAY;
     imageAppStore = APPSTORE;
+    red = RED;
 }
