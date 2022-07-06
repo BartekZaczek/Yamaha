f03528a05499e750a6c60e9f53f341a42b5560b1
BartekZaczek <bartosz_zaczek@o2.pl>
2022-07-06 12:30:58 +0200

    some changes

--- force-app/main/default/lwc/site/site.js
+++ force-app/main/default/lwc/site/site.js
@@ -6,10 +6,10 @@ export default class Site extends LightningElement {
 
     
     get backgroundGrey(){
-        return `height:auto; width:auto; background-image:url(${GREY})`;
+        return `height:220px; width:auto; background-image:url(${GREY})`;
     }
 
     get backgroundBlue(){
-        return `height:200px; width:auto; background-image:url(${BLUE})`;
+        return `height:220px; width:auto; background-image:url(${BLUE})`;
     }
 }
