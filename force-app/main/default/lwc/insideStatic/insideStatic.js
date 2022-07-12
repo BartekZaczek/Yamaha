1274231438a4e8457dbdaabd79a8c4baca0cb31f
BartekZaczek <bartosz_zaczek@o2.pl>
2022-07-12 20:19:13 +0200

    test

--- force-app/main/default/lwc/insideStatic/insideStatic.js
+++ force-app/main/default/lwc/insideStatic/insideStatic.js
@@ -2,9 +2,13 @@ import { LightningElement } from 'lwc';
 import GOOGLEPLAY from '@salesforce/resourceUrl/googlePLay';
 import APPSTORE from '@salesforce/resourceUrl/appStore';
 import RED from '@salesforce/resourceUrl/red';
+import GREY from '@salesforce/resourceUrl/Grey';
 
 export default class InsideStatic extends LightningElement {
     imageGoogle = GOOGLEPLAY;
     imageAppStore = APPSTORE;
     red = RED;
+    get backgroundGrey(){
+        return `height:250px; width:auto; background-image:url(${GREY})`;
+    }
 }
