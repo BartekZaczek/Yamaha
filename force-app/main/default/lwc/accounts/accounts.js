cad926fe1de504f4704fffe4ef40e45799b825e7
BartekZaczek <bartosz_zaczek@o2.pl>
2022-08-22 20:26:34 +0200

    ;

--- force-app/main/default/lwc/accounts/accounts.js
+++ force-app/main/default/lwc/accounts/accounts.js
@@ -2,7 +2,7 @@ import { LightningElement, wire } from 'lwc';
 import getAccounts from '@salesforce/apex/FetchData.getAccounts';
 
 export default class Accounts extends LightningElement {
-     acctData;
+     acctData
      visibleAcct
     @wire(getAccounts) accts({error, data}){
         if(data){
