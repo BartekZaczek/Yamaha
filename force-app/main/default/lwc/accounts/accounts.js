ad053b012daba95fed0a66d8d8e45a5334ceba92
BartekZaczek <bartosz_zaczek@o2.pl>
2022-08-22 20:25:43 +0200

    end pagination

--- force-app/main/default/lwc/accounts/accounts.js
+++ force-app/main/default/lwc/accounts/accounts.js
@@ -2,12 +2,18 @@ import { LightningElement, wire } from 'lwc';
 import getAccounts from '@salesforce/apex/FetchData.getAccounts';
 
 export default class Accounts extends LightningElement {
-    acctData;
+     acctData;
+     visibleAcct
     @wire(getAccounts) accts({error, data}){
         if(data){
             this.acctData = data;
+            console.log(this.acctData)
         }else if(error){
             this.error = error;
         }
     }
+
+    updateAccountHandler(event){
+        this.visibleAcct = [...event.detail.records]
+        console.log(event.detail.records)}
 }
--- force-app/main/default/lwc/pagination/pagination.js
+++ force-app/main/default/lwc/pagination/pagination.js
@@ -1,3 +1,56 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Pagination extends LightningElement {}
+export default class Pagination extends LightningElement {
+    totalRecords
+    @api recordSize = 2
+    totalPage = 0
+    currentPage = 1
+
+    get records(){
+        return this.visibleRecords
+    }
+
+    @api
+    set records(data){
+        if(data){
+            this.totalRecords = data;
+            this.recordSize = Number(this.recordSize);
+            this.totalPage = Math.ceil(data.length/this.recordSize);
+            this.updateRecords()
+        }
+        
+    }
+
+    updateRecords(){
+        const start = (this.currentPage-1)*this.recordSize
+        const end = this.recordSize*this.currentPage
+        this.visibleRecords = this.totalRecords.slice(start, end)
+        this.dispatchEvent(new CustomEvent('update',{
+            detail:{
+                records:this.visibleRecords
+            }
+        }))
+    }
+
+    nextHandler(){
+        if(this.currentPage < this.totalPage){
+            this.currentPage = this.currentPage+1
+            this.updateRecords()
+        }
+    }
+
+    previousHandler(){
+        if(this.currentPage > 1){
+            this.currentPage = this.currentPage-1
+            this.updateRecords()
+        }
+    }
+
+    get disableNext(){
+        return this.currentPage >= this.totalPage
+    }
+
+    get disablePrevious(){
+        return this.currentPage <= 1
+    }
+}
