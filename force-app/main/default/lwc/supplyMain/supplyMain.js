6bbf1868dd1df7d80ca5963efd1807e68ed91ae0
BartekZaczek <bartosz_zaczek@o2.pl>
2022-08-25 21:58:20 +0200

    starting combobox with apex

--- force-app/main/default/lwc/supplyMain/supplyMain.js
+++ force-app/main/default/lwc/supplyMain/supplyMain.js
@@ -1,17 +1,36 @@
-import { LightningElement } from 'lwc';
-
+import getAcrylic from '@salesforce/apex/AcrylicController.getAcrylic';
+import { LightningElement, wire, track } from 'lwc';
+let i=0;
 export default class SupplyMain extends LightningElement {
-    value = 'inProgress';
+    @track items = []; //this will hold key, value pair
+    @track value = ''; //initialize combo box value
+    @track chosenValue = '';
+    aData
 
-    get options() {
-        return [
-            { label: 'New', value: 'new' },
-            { label: 'In Progress', value: 'inProgress' },
-            { label: 'Finished', value: 'finished' },
-        ];
+    @wire(getAcrylic) acrylics({error, data}){
+        if(data){
+            this.aData = data;
+            console.log(this.aData);
+            for(i=0; i<data.lenght;i++) {
+                this.items = data.map(items => ({ label: data[i].Name, value: data[i].Id }));
+            }
+            console.log(this.items);
+        }else if(error){
+            this.error = error;
+        }
+    }
+
+    get acrylicOptions(){
+        return this.items;
     }
 
     handleChange(event) {
-        this.value = event.detail.value;
+        const selectedOption = event.detail.value;
+        console.log('selected value=' + selectedOption);
+        this.chosenValue = selectedOption;
+    }
+
+    get selectedValue(){
+        return this.chosenValue;
     }
 }
