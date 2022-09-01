277f38b8e7d84712dfa6a4638f799a187b3560d9
BartekZaczek <bartosz_zaczek@o2.pl>
2022-09-02 00:05:38 +0200

    backend new lwc

--- force-app/main/default/lwc/acrylicPlates/acrylicPlates.js
+++ force-app/main/default/lwc/acrylicPlates/acrylicPlates.js
@@ -1,18 +1,52 @@
-import { LightningElement } from 'lwc';
+import getAcrylic from '@salesforce/apex/AcrylicController.getAcrylic';
+import { LightningElement, track, wire } from 'lwc';
 
 export default class AcrylicPlates extends LightningElement {
 
-    get options(){
-        return [
+    @track optionsArray = [];
+    @track value = '';
+    @track label = '';
+    @track quantity = 0;
+    @track quantityToShow = 0;
+
+    @wire(getAcrylic)
+        acrylics;
+
+    get optionsAddDelete() {
+         return [
             { label: 'Add', value: 'add' },
-            { label: 'Delete', value: 'delete'},
+            { label: 'Delete', value: 'delete' },
         ];
+    }    
+
+    connectedCallback(){
+        getAcrylic()
+        .then( result => {
+            let arr = [];
+            for(var i=0; i<result.length; i++){
+                arr.push({label : result[i].Name, value : result[i].Id, quantity : result[i].Quantity__c})
+                console.log(arr[i])
+            }
+            this.optionsArray = arr;
+        })
     }
 
-    get optionsAcrylic(){
-        return [
-            { label: 'White HG', value: 'hg' },
-            { label: 'White MAT', value: 'mat'},
-        ];
+   
+
+    handleChange(event){
+        this.value = event.detail.value;
+        for(var i = 0; i<this.optionsArray.length; i++){
+            if(this.value == this.optionsArray[i].value){
+                this.quantity = this.optionsArray[i].quantity;
+                this.quantityToShow = this.quantity;
+            }
+        }
     }
+
+    handleClick(){
+        const fields = {};
+        fields[ID_FIELD.fieldApiName] = this.value;
+        
+    }
+    
 }
