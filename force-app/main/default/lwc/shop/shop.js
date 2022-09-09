f879c70b3275dd19f0d7e6f82e0d8e86b21b3c04
BartekZaczek <bartosz_zaczek@o2.pl>
2022-09-09 19:02:11 +0200

    /

--- force-app/main/default/lwc/shop/shop.js
+++ force-app/main/default/lwc/shop/shop.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import WHITE_MAT from '@salesforce/resourceUrl/whiteMat';
+import WHITE_HG from '@salesforce/resourceUrl/whiteHg';
+import BLACK_MAT from '@salesforce/resourceUrl/blackMat';
 
-export default class Shop extends LightningElement {}
+export default class Shop extends LightningElement {
+    whiteHg = WHITE_HG;
+    whiteMat = WHITE_MAT;
+    blackMat = BLACK_MAT;
+}
