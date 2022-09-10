772b0ad02188d221bd94a453c527858538376480
BartekZaczek <bartosz_zaczek@o2.pl>
2022-09-10 11:26:48 +0200

    add css

--- force-app/main/default/lwc/shop/shop.js
+++ force-app/main/default/lwc/shop/shop.js
@@ -3,6 +3,7 @@ import WHITE_MAT from '@salesforce/resourceUrl/whiteMat';
 import WHITE_HG from '@salesforce/resourceUrl/whiteHg';
 import BLACK_MAT from '@salesforce/resourceUrl/blackMat';
 
+
 export default class Shop extends LightningElement {
     whiteHg = WHITE_HG;
     whiteMat = WHITE_MAT;
