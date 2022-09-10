07d7a0db0dec74a647904364ec46afc7a0a102ef
BartekZaczek <bartosz_zaczek@o2.pl>
2022-09-10 20:20:38 +0200

    start2 nav

--- force-app/main/default/lwc/nav/nav.js
+++ force-app/main/default/lwc/nav/nav.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class Nav extends LightningElement {}
+export default class Nav extends LightningElement {
+     basketLable = "basket : ";
+     basketSize = 0;
+     basket = this.basketLable + ' ' + this.basketSize.toString();
+
+    
+}
