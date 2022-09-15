cd0aa58adec69eaaae9dcf8a834fe95c8ba98935
BartekZaczek <bartosz_zaczek@o2.pl>
2022-09-15 18:56:49 +0200

    ...

--- force-app/main/default/lwc/shop/shop.js
+++ force-app/main/default/lwc/shop/shop.js
@@ -1,45 +1,53 @@
 import { LightningElement, track } from 'lwc';
-import WHITE_MAT from '@salesforce/resourceUrl/whiteMat';
+import ANTHRACITE_GREY from '@salesforce/resourceUrl/anthraciteGrey';
 import WHITE_HG from '@salesforce/resourceUrl/whiteHg';
 import BLACK_MAT from '@salesforce/resourceUrl/blackMat';
 import getAcrylic from '@salesforce/apex/AcrylicController.getAcrylic';
 
 
 export default class Shop extends LightningElement {
-    whiteHg = WHITE_HG;
-    whiteMat = WHITE_MAT;
-    blackMat = BLACK_MAT;
-    imgColor = this.whiteHg
+    @track imgColor = WHITE_HG;
     @track colors = [];
-    imgColor = '';
-    staticList = [];
+    @track mapWithImg = [];
+    tempCol  =  '';
     valueCombo = '';
-    colorToShow;
-
+    
     connectedCallback(){
         getAcrylic()
         .then( result => {
             let arr = [];
-
+            let map = new Map();
             for(var i = 0; i < result.length; i++){
-                
-                arr.push({label : result[i].Name, value : result[i].Id, color : result[i].acrylicColor__c})
-                console.log(arr[i]);
+                switch (result[i].Name){
+                    case  'Black Mat' :
+                        this.tempCol = BLACK_MAT;
+                        break;
+                    case 'White HG':
+                        this.tempCol = WHITE_HG;
+                        this.valueCombo = result[i].Name;
+                        console.log(this.valueCombo)
+                        break;
+                    case 'Anthracite Grey':
+                        this.tempCol = ANTHRACITE_GREY;
+                        break;
+                    
+                }
+                map.set(result[i].Name,  this.tempCol);
+                arr.push({label : result[i].Name, value : result[i].Id})
             }
-            this.valueCombo = arr[0];
             this.colors = arr;
-            this.colorToShow = arr[0].color;
-            console.log(this.colorToShow);
+            this.mapWithImg = map;  
         })
     }
 
-    handleChangeColors(event){
+     handleChangeColors(event){
         for(var i = 0; i < this.colors.length; i++){
             if( event.detail.value == this.colors[i].value){
-                this.imgColor = this.whiteHg;
+                this.imgColor = this.mapWithImg.get(this.colors[i].label);
+                console.log(this.mapWithImg.get(this.colors[i].label))
             }
+            
+           
         }
-       
     }
-
 }
